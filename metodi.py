40f6cbb188a3fe3450ca9f0f1f77cb15850ea808
russu10 <s312556@studenti.polito.it>
2025-06-25 12:53:27 +0200

    prima soluzione

--- metodi.py
+++ metodi.py
@@ -13,3 +13,17 @@ self._graph.in_edges(n, data=True):
 self._graph.neighbors(start)
 #trova i nodi vicini  uscenti in un grafo orientato
 #iìtrova tutti gli adiacenti in un grafo normale
+
+conn = nx.node_connected_component(self.graph, source)
+        print("modo 4", len(conn))
+        return len(conn)
+
+#trova la componente connessa in un grafo NON ORIENTATO
+
+nx.weakly_connected_components(grafo)
+nx.strongly_connected_components(grafo)
+
+for componente in nx.strongly_connected_components(grafo):
+    if nodoCercaro in componente:
+        componenteTrovata = componente.copy.deepcopy()
+#comp connessa per un grafo ORIENTATO
