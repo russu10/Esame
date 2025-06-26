4a108bbb09677743a7cab617864d57d929d11a74
russu10 <s312556@studenti.polito.it>
2025-06-26 11:03:44 +0200

    Update metodi_sql.py

--- metodi_sql.py
+++ metodi_sql.py
@@ -3,3 +3,7 @@
 
 """year(date)"""
 #trova l anno in un datetime
+
+Il metodo fetchall() restituisce tutte le righe del risultato di una query SQL eseguita con cursor.execute(...).
+
+Restituisce una lista di tuple, dove ogni tupla rappresenta una riga.
