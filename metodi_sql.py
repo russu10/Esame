2d0c6f2689eb8f2896f2ace1143ce8f5e65aa185
russu10 <s312556@studenti.polito.it>
2025-07-05 15:43:41 +0200

    aggiustato

--- metodi_sql.py
+++ metodi_sql.py
@@ -3,3 +3,17 @@
 
 """year(date)"""
 #trova l anno in un datetime
+
+"""SELECT>T g1.GeneID AS id1, g2.GeneID AS id2,
+             CASE 
+                 WHEN g1.Chromosome = g2.Chromosome THEN g1.Chromosome
+                 ELSE g1.Chromosome + g2.Chromosome
+             END AS somma
+      FROM genes g1, genes g2
+      WHERE g1.Chromosome <= g2.Chromosome
+      GROUP BY g1.GeneID, g2.GeneID"""
+# if
+"""CASE
+WHEN EVENTO 1 THEN SOLUZIONE 1
+ELSE SOLUZIONE 2
+END"""
